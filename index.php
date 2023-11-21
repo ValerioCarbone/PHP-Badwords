50d5a748ac8dd92104a90cd6ecacaa2677f4f67f
ValerioCarbone <valeriocarbone10@gmail.com>
2023-11-21 15:32:03 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Form</title>
+</head>
+<body>
+    <form action="word_censor.php" method="POST">
+        <input type="text" name="replace_word">
+    </form>
+</body>
+</html>
